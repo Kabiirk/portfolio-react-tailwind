8f938cfe30d206dd26209bb6a9044becac7ca430
Kabiir <kabiirk@gmail.com>
2022-05-27 02:28:39 +0530

    Landing Page all elements placed, minor adjustments left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import Name_Logo from '../assets/Name_Logo.png'
 
 const Hero = () => {
   return (
-    <div name='hero' className='w-full h-screen bg-[#0a192f]'>
+    <div name='hero' className='w-full h-screen bg-[#0a192f] flex'>
+        <div className='ml-5 my-auto flex-col invisible sm:visible'>
+          <img className='w-5 pb-1' alt='Github' src={Name_Logo}/>
+          <img className='w-5 py-1' alt='LinkedIn' src={Name_Logo}/>
+          <img className='w-5 pt-1' alt='Behance' src={Name_Logo}/>
+        </div>
         {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full place-items-center'>
+        <div className='max-w-[1000px] mx-auto px-10 flex flex-col justify-center h-full place-items-center text-center'>
             <p className='text-pink-600 py-1'>Hi, my name is</p>
             <h1 className='text-3xl sm:text-5xl font-bold text-[#ccd6f6] py-1'>Kabiir Krishna</h1>
             <h2 className='text-3xl sm:text-5xl font-bold text-[#8892b0] py-1'>I build awesome things.</h2>
             <div className='pt-5'>
                 <button className='text-white rounded-md flex items-center border-2 px-6 py-3 hover:bg-pink-600 hover:border-pink-600'>Check out my website !</button>
             </div>
-
         </div>
-
+        <div className='mr-5 my-auto flex-col items-center invisible sm:visible'>
+          <img className='w-5 pb-1' alt='Github' src={Name_Logo}/>
+          <img className='w-5 py-1' alt='LinkedIn' src={Name_Logo}/>
+          <img className='w-5 pt-1' alt='Behance' src={Name_Logo}/>
+        </div>
     </div>
   )
 }
