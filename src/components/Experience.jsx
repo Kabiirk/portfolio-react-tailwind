04c7741da3827af1f5dad14588f87a60243a4c11
Kabiir <kabiirk@gmail.com>
2022-06-04 02:55:22 +0530

    Experience - Desired responsive grid achieved, improvements req.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Experience = () => {
   return (
     <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-        <div className='flex flex-col justify-center items-center w-full h-full '>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
           <div className='max-w-[1000px] w-full grid grid-cols-1'>
             <div className='text-center pb-8 pl-4'>
               <p className='text-3xl inline font-calibre-sb'>
@@ -15,15 +15,17 @@ const Experience = () => {
           </div>
 
           {/* Should be Tabs */}
-          <div className='max-w-[750px] w-full grid sm:grid-cols-2 gap-8 px-4 bg-blue-500'>
-              <div className='text-center md:flex-col lg:felx-col flex pl-10 bg-emerald-400'>
-                  <div className='bg-red-400'>A</div>
-                  <div className='bg-red-500'>B</div>
-                  <div className='bg-red-600'>C</div>
-                  <div className='bg-red-700'>D</div>
+          <div className='max-w-[1000px] w-full grid md:grid-cols-4 grid-col-1 gap-8 px-10'>
+              <div className='text-left md:flex-col flex sm:items-stretch overflow-x-scroll gap-2 col-span-1 sm:col-span-1 font-sf-mono-re'>
+                  <div className='bg-red-400'>PwC India</div>
+                  <div className='bg-red-500'>IIT-Roorkee</div>
+                  <div className='bg-red-600'>IBM</div>
+                  <div className='bg-red-700'>Deloitte</div>
+                  <div className='bg-red-800'>Microsoft SP</div>
+                  <div className='bg-red-900'>CSI-VIT</div>
               </div>
 
-              <div className='font-calibre-re'>
+              <div className='font-calibre-re md:col-span-3 col-span-1'>
                 <p>
                   A 2021 B.Tech graduate, seeking opportunities in domains like
                   (A).Automation with IoT, (B).Cyber Security,(C).Strategies to integrate
