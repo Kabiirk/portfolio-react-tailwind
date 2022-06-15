75e0ed155d2ef8424fb9c6664ff2d48b5c8af1f4
Kabiir <kabiirk@gmail.com>
2022-06-16 02:45:40 +0530

    Implemented onScroll animation for About section, others WIP

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 // import Name_Logo from '../assets/Name_Logo.png'
 import profile from '../assets/profile.jpg'
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+const imagevariant = {
+  hidden: { y:50, opacity:0 },
+  visible: { y:0, opacity:1, transition:{duration:0.6} },
+}
 
 const About = () => {
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+
+    useEffect(() => {
+      if (inView) {
+        control.start("visible");
+      } 
+    }, [control, inView]);
+
     return (
       <div name='about' className='w-full h-screen bg-background text-heading'>
         <div className='flex flex-col justify-center items-center w-full h-full '>
@@ -13,11 +30,15 @@ const About = () => {
               </p>
             </div>
           </div>
-            <div className='max-w-[800px] w-full grid sm:grid-cols-2 gap-4 px-4'>
+            <motion.div className='max-w-[800px] w-full grid sm:grid-cols-2 gap-4 px-4'
+                        ref={ref}
+                        variants={imagevariant}
+                        initial='hidden'
+                        animate={control}>
               <div className='content-right'>
                 {/* <img src={Name_Logo} alt="My pic" className='mx-auto'/> */}
                 {/* <svg className='w-[16%] mx-auto' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M5,0H45a5,5,0,0,1,5,5V45a5,5,0,0,1-5,5H5a5,5,0,0,1-5-5V5A5,5,0,0,1,5,0Z" fill="#64ffda" fill-rule="evenodd"/><g style={{isolation:"isolate"}}><g style={{isolation:"isolate"}}><path d="M21.33,23.92l8.08-9.67h5.43l-8.19,9.63,8.5,11.87H29.73L23.36,27l-2,2.42v6.37H16.85V14.25h4.48Z" fill="#0a192f"/></g></g></g></g></svg> */}
-                <img src={profile} alt="Me !" className='mx-auto rounded-md w-[85%]'/>
+                <img src={profile} alt="Me !" title="Smiley face" className='mx-auto rounded-md w-[85%]'/>
               </div>
               <div className='font-calibre-re text-base'>
                 <p>
@@ -38,7 +59,7 @@ const About = () => {
                 <button className='border-2 rounded-md px-6 py-3 text-sm font-sf-mono-re justified-center text-highlight border-highlight hover:bg-[#64ffda] hover:bg-opacity-10 hover:duration-200'>Download my Resume !</button>
                 </div>
               </div>
-            </div>
+            </motion.div>
         </div>
       </div>
     );
